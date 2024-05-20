b60171e90c924dcf46e658f869512f2a237218c8
jungseonw00 <laborlawseon@gmail.com>
2024-05-20 20:58:26 +0900

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,83 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.2.5"
+	id("io.spring.dependency-management") version "1.1.4"
+	id("org.asciidoctor.jvm.convert") version "3.3.2"
+	kotlin("jvm") version "1.9.23"
+	kotlin("plugin.spring") version "1.9.23"
+	kotlin("plugin.jpa") version "1.9.23"
+	kotlin("plugin.allopen") version "1.9.23"
+	kotlin("kapt") version "1.9.23"
+	idea
+}
+
+group = "wanted.market"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["snippetsDir"] = file("build/generated-snippets")
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("javax.xml.bind:jaxb-api:2.4.0-b180830.0359")
+	implementation("com.querydsl:querydsl-jpa:5.1.0:jakarta")
+	implementation("jakarta.persistence:jakarta.persistence-api")
+	implementation("jakarta.annotation:jakarta.annotation-api")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("io.github.microutils:kotlin-logging-jvm:4.0.0-beta-2")
+	implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.9.1")
+
+	runtimeOnly("com.mysql:mysql-connector-j")
+
+	kapt("jakarta.annotation:jakarta.annotation-api")
+	kapt("jakarta.persistence:jakarta.persistence-api")
+	kapt("org.projectlombok:lombok")
+	kapt("com.querydsl:querydsl-apt:5.1.0:jakarta")
+	kapt("org.springframework.boot:spring-boot-configuration-processor")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+	testImplementation("io.kotest:kotest-assertions-core:5.9.0")
+	testImplementation("io.kotest:kotest-runner-junit5:5.9.0")
+	testImplementation("io.mockk:mockk:1.13.10")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testCompileOnly("io.github.microutils:kotlin-logging-jvm:4.0.0-beta-2")
+}
+
+allOpen {
+	annotation("jakarta.persistence.Entity")
+	annotation("jakarta.persistence.Embeddable")
+	annotation("jakarta.persistence.MappedSuperclass")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.test {
+	outputs.dir(project.extra["snippetsDir"]!!)
+}
+
+tasks.asciidoctor {
+	inputs.dir(project.extra["snippetsDir"]!!)
+	dependsOn(tasks.test)
+}
