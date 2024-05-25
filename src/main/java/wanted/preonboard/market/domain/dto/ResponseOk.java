b1ee13128e3573742121a8fda446da35632e266b
HanDaWoon <bookpia.han@gmail.com>
2024-05-25 21:22:44 +0900

    refactor: 응답 객체 리팩토링

--- src/main/java/wanted/preonboard/market/domain/dto/ResponseOk.java
+++ src/main/java/wanted/preonboard/market/domain/dto/ResponseOk.java
@@ -1,22 +1,21 @@
 package wanted.preonboard.market.domain.dto;
 
 import org.springframework.http.ResponseEntity;
-import wanted.preonboard.market.domain.entity.Member;
 import wanted.preonboard.market.message.ResponseMessage;
 
 import java.util.Map;
 
 public record ResponseOk(Object body) {
     public ResponseEntity<Map<String, Object>> toResponse() {
-        if (body.getClass().equals(Member.class)) {
+        if (body.getClass().equals(String.class)) {
             return ResponseEntity.ok(Map.of(
                 ResponseMessage.STATUS.getKey(), ResponseMessage.SUCCESS.getKey(),
-                ResponseMessage.DATA.getKey(), body
+                ResponseMessage.MESSAGE.getKey(), body
             ));
         }
         return ResponseEntity.ok(Map.of(
             ResponseMessage.STATUS.getKey(), ResponseMessage.SUCCESS.getKey(),
-            ResponseMessage.MESSAGE.getKey(), body
+            ResponseMessage.DATA.getKey(), body
         ));
     }
 }
