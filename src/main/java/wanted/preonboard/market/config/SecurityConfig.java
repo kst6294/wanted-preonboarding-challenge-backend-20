7f966cf5892850ce6f50acebde47395c77b37ab6
HanDaWoon <bookpia.han@gmail.com>
2024-05-25 21:26:02 +0900

    feat: csrf disable 처리

--- src/main/java/wanted/preonboard/market/config/SecurityConfig.java
+++ src/main/java/wanted/preonboard/market/config/SecurityConfig.java
@@ -4,20 +4,18 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import wanted.preonboard.market.filter.CsrfCookieFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -39,21 +37,15 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new CsrfTokenRequestAttributeHandler();
-        csrfTokenRequestAttributeHandler.setCsrfRequestAttributeName("_csrf");
-
         http
             .securityContext(securityContext -> securityContext.requireExplicitSave(false))
             .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
             .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-            .csrf(csrf ->
-                csrf.csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
-                    .ignoringRequestMatchers("/register")
-                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-            )
-            .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+            .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests((requests) -> requests
-//                .requestMatchers("/user").authenticated()
+                .requestMatchers("/user").authenticated()
+                .requestMatchers(HttpMethod.GET, "/products").permitAll()
+                .requestMatchers("/products", "/products/*").authenticated()
                 .anyRequest().permitAll()
             )
             .formLogin(Customizer.withDefaults())
