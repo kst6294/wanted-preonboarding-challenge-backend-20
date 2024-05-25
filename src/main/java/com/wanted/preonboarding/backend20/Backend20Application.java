552cb9bf4c695ca5e8f34f66b51c5f1250fdce76
mohyerolo <garam2101@naver.com>
2024-05-25 15:05:07 +0900

    chore: 프로젝트 준비

---
+++ src/main/java/com/wanted/preonboarding/backend20/Backend20Application.java
@@ -0,0 +1,13 @@
+package com.wanted.preonboarding.backend20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backend20Application.class, args);
+	}
+
+}
