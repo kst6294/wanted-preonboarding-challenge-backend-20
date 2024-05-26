32bb148c88b8d161856176ed34b078f90de65e0d
NarciSource <jwch11@gmail.com>
2024-05-26 23:47:54 +0900

    fix: Bearer 토큰 처리

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,9 @@ import jwt from "jsonwebtoken";
 
 function verifyToken(req, res, next) {
     try {
-        req.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+        const token = req.headers.authorization.replace("Bearer ", "");
+
+        req.decoded = jwt.verify(token, process.env.JWT_SECRET);
         return next();
     } catch (error) {
         res.status(403).send("자격 증명 실패");
