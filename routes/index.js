f93db515bd94dc5f206321c58fa966f673d25db4
NarciSource <jwch11@gmail.com>
2024-05-26 14:45:11 +0900

    feat: products get/post 구현

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,53 @@
-import { Router } from 'express';
+import { Router } from "express";
+import dbClient from "../db/dbClient.js";
+
 var router = Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/products", async function (req, res, next) {
+    const table = "products";
+
+    try {
+        const item = await dbClient(table);
+
+        res.setHeader("Content-Type", "application/json");
+        res.json(item);
+    } catch (_) {
+        const error = new Error("No exist.");
+        error.status = 404;
+        return next(error);
+    }
+});
+
+router.get("/products/:product_id", async function (req, res, next) {
+    const table = "products";
+    const { product_id } = req.params;
+
+    try {
+        const item = await dbClient(table).where("ID", product_id).first();
+
+        res.setHeader("Content-Type", "application/json");
+        res.json(item);
+    } catch (_) {
+        const error = new Error("No exist.");z
+        error.status = 404;
+        return next(error);
+    }
+});
+
+router.post("/products", async function (req, res, next) {
+    const table = "products";
+    const post_dat = req.body;
+
+    try {
+        await dbClient(table).insert({
+            ReservationState: "Available",
+            ...post_dat,
+        });
+        res.end("successful");
+    } catch (_) {
+        error.status = 404;
+        return next(error);
+    }
 });
 
 export default router;
