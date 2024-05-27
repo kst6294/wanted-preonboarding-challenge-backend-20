bc72b4961affc859a5b1a8bd3f2c6973ce181a02
NarciSource <jwch11@gmail.com>
2024-05-27 19:20:38 +0900

    sql: 트리거 추가
    - 제품 구매 수량에 따른 제품 상태 트리거
    - 주문 상태에 따른 제품 상태 트리거

--- db/query.sql
+++ db/query.sql
@@ -2,21 +2,19 @@
 DROP TABLE IF EXISTS Products;
 DROP TABLE IF EXISTS Users;
 DROP TABLE IF EXISTS Orders;
-
 -- 외래 키 지원 활성
 PRAGMA foreign_keys = ON;
-
 -- 유저 테이블
 CREATE TABLE Users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL,
     password INTEGER NOT NULL
 );
-
 -- 유저 삽입
-INSERT INTO Users (Username, Password) VALUES ('user', 'pswd');
-INSERT INTO Users (Username, Password) VALUES ('tester', 'pswd');
-
+INSERT INTO Users (Username, Password)
+VALUES ('user', 'pswd');
+INSERT INTO Users (Username, Password)
+VALUES ('tester', 'pswd');
 -- 제품 테이블
 CREATE TABLE Products (
     product_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -27,7 +25,6 @@ CREATE TABLE Products (
     status TEXT NOT NULL CHECK (status IN ('Available', 'Reserved', 'SoldOut')),
     FOREIGN KEY (seller_id) REFERENCES Users(id)
 );
-
 -- 주문 테이블
 CREATE TABLE Orders (
     buyer_id INT,
@@ -38,3 +35,27 @@ CREATE TABLE Orders (
     FOREIGN KEY (buyer_id) REFERENCES Users(id),
     FOREIGN KEY (product_id) REFERENCES Products(product_id)
 );
+-- 제품 구매 수량에 따른 제품 상태 트리거
+CREATE TRIGGER update_status_reserved
+AFTER
+UPDATE OF amount ON Products
+    WHEN NEW.amount = 0 BEGIN
+UPDATE Products
+SET status = 'Reserved'
+WHERE product_id = NEW.product_id;
+END;
+-- 주문 상태에 따른 제품 상태 트리거
+CREATE TRIGGER update_status_soldout
+AFTER
+UPDATE OF status ON Orders
+    WHEN NEW.status = 'Confirm' BEGIN
+UPDATE Products
+SET status = 'SoldOut'
+WHERE Products.product_id == NEW.product_id
+    AND NOT EXISTS (
+        SELECT *
+        FROM Orders
+        WHERE Orders.product_id == NEW.product_id
+            AND status != 'Confirm'
+    );
+END;
