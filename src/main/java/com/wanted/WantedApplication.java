cf7e22cb8c71846e22c8c02cd20c75e4391b100d
junghunchoi <azzudi@gmail.com>
2024-05-27 16:01:42 +0900

    [2024.05.27] 최초 커밋

---
+++ src/main/java/com/wanted/WantedApplication.java
@@ -0,0 +1,13 @@
+package com.wanted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WantedApplication.class, args);
+	}
+
+}
