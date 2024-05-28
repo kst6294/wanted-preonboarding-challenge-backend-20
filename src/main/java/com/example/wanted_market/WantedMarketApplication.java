2b44bd6c31ce4030e62e8598cafbc66166c5596e
Lee Seung Jin <lpromotion00@gmail.com>
2024-05-28 18:28:13 +0900

    init

---
+++ src/main/java/com/example/wanted_market/WantedMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.wanted_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WantedMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wanted_market/WantedMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wanted_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
