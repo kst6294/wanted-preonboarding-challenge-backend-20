b968d50591d967f6ccfa787e7a071469026f9767
seungbin.kim <seungbin.kim.dev@gmail.com>
2024-05-28 21:10:55 +0900

    Initial Commit

---
+++ src/main/java/kr/co/wanted/market/market/MarketApplication.java
@@ -0,0 +1,13 @@
+package kr.co.wanted.market.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/wanted/market/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.wanted.market.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
