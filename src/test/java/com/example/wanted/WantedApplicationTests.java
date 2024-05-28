7b2e7cfee0ec91fbeb6e6c5d34864889bfaff568
moonjin-kim <ace39418@gmail.com>
2024-05-28 22:17:29 +0900

    init: firstCommit

---
+++ src/test/java/com/example/wanted/WantedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wanted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
