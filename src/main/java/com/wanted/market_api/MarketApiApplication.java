c68f4c3f56c96b0a4abe4cf0aeaa5d002d24bb0d
Inseok Chang <inni.chang95@gmail.com>
2024-05-28 13:34:38 +0900

    initial commit

---
+++ src/main/java/com/wanted/market_api/MarketApiApplication.java
@@ -0,0 +1,13 @@
+package com.wanted.market_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wanted/market_api/MarketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.market_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
