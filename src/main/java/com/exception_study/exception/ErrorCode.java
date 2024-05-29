610f4ec467a7099e921aeb0c8ff4356c2400748d
engh0205@naver.com <engh0205@naver.com>
2024-05-29 17:23:12 +0900

    feat: 예외처리
    기본적인 예외들을 처리하기 위한 클래스를 구현했다.

---
+++ src/main/java/com/exception_study/exception/ErrorCode.java
@@ -0,0 +1,19 @@
+package com.exception_study.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Invalid token"),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not founded"),
+    PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND,"Product not founded"),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Invalid password"),
+    INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "User has invalid permission"),
+    DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Database error occurs");
+
+    private final HttpStatus status;
+    private final String message;
+}
