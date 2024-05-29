774eabfbdc249573558db803755551e48dd807be
ydj515 <ydj515@hanmail.net>
2024-05-29 22:34:45 +0900

    init project

---
+++ src/test/java/org/example/preonboarding/PreonboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.preonboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreonboardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
