33f6a8ac72ff70ff5b6d2a4552cd46b05ae2c794
ydj515 <ydj515@hanmail.net>
2024-05-30 00:33:29 +0900

    feat: jpa auditing config 추가

---
+++ src/main/java/org/example/preonboarding/config/auditing/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package org.example.preonboarding.config.auditing;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
