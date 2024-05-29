245030f199a4ebdedd325b2ae91b468d3ea08025
engh0205@naver.com <engh0205@naver.com>
2024-05-29 17:23:31 +0900

    init: init

---
+++ src/main/java/com/exception_study/ExceptionStudyApplication.java
@@ -0,0 +1,13 @@
+package com.exception_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExceptionStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exception_study/ExceptionStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exception_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
