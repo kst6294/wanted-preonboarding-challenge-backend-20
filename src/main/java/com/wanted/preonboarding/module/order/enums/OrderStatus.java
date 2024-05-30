42391f6e3ebaee6c2d0e44c8471c7194dad5f69c
RyuSangwon <rsw2@connectly.co.kr>
2024-05-30 09:54:47 +0900

    enum html 폴더 이동

--- src/main/java/com/wanted/preonboarding/module/order/enums/OrderStatus.java
+++ src/main/java/com/wanted/preonboarding/module/order/enums/OrderStatus.java
@@ -1,13 +1,19 @@
 package com.wanted.preonboarding.module.order.enums;
 
 import com.wanted.preonboarding.module.common.enums.EnumType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
 public enum OrderStatus implements EnumType {
 
-    ORDERED,
-    COMPLETED,
-    SETTLEMENT
+    ORDERED("판매중"),
+    COMPLETED("예약중"),
+    SETTLEMENT("완료")
     ;
+
+    private final String displayName;
 
     @Override
     public String getName() {
