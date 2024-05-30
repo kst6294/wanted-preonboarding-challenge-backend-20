33906644d33ec78c5812178cf72038a2071d087d
HBLEEEEE <lhbs0528@gmail.com>
2024-05-30 22:49:21 +0900

    fist

---
+++ src/main/java/com/want/BEProject/BeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.want.BEProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/want/BEProject/BeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.want.BEProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
