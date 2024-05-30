aa5f464d90cfbf5f8d2eac94d08ea24b1b241d73
ydj515 <ydj515@hanmail.net>
2024-05-30 23:52:32 +0900

    feat: exception handler 추가(bindException, SQLIntegrityConstraintViolationException)

--- src/main/java/org/example/preonboarding/common/controllerAdvices/GlobalExceptionHandler.java
+++ src/main/java/org/example/preonboarding/common/controllerAdvices/GlobalExceptionHandler.java
@@ -5,8 +5,12 @@ import org.example.preonboarding.common.payload.ApiError;
 import org.example.preonboarding.member.exception.WithdrawException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -16,6 +20,28 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
                 ApiError.builder()
                         .resultCode(ResultCode.WITHDRAW_FAIL)
+                        .data(ex.getMessage())
+                        .build()
+        );
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<?> bindException(BindException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                ApiError.builder()
+                        .resultCode(ResultCode.FAIL)
+                        .data(ex.getBindingResult().getAllErrors().get(0).getDefaultMessage())
+                        .build()
+        );
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<?> bindException(SQLIntegrityConstraintViolationException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                ApiError.builder()
+                        .resultCode(ResultCode.FAIL)
                         .data(ex.getMessage())
                         .build()
         );
