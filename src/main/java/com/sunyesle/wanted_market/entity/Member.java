f6c18be78a6ad1fb9562861e86ff5be0846b81ea
sunyesle <sunyesle@gmail.com>
2024-05-30 20:48:10 +0900

    feat: 사용자 entity 추가

---
+++ src/main/java/com/sunyesle/wanted_market/entity/Member.java
@@ -0,0 +1,17 @@
+package com.sunyesle.wanted_market.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
