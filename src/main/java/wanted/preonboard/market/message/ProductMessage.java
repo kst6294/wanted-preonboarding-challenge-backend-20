22e5090a2d9b10143200b1acb0a4bc9a14db0ccf
HanDaWoon <bookpia.han@gmail.com>
2024-05-30 14:24:12 +0900

    feat: 제품 응답 메세지 추가

--- src/main/java/wanted/preonboard/market/message/ProductMessage.java
+++ src/main/java/wanted/preonboard/market/message/ProductMessage.java
@@ -7,6 +7,7 @@ public enum ProductMessage {
     ERROR("제품 등록 중 알 수 없는 오류가 발생했습니다."),
     PRODUCT_NOT_FOUND("제품을 찾을 수 없습니다."),
     PRODUCT_ALREADY_EXISTS("이미 존재하는 제품입니다."),
+    PRODUCT_ALREADY_SOLD("이미 판매된 제품입니다."),
     PRODUCT_NOT_AUTHORIZED("제품 권한이 없습니다."),
     INVALID_PRODUCT_STATE("유효하지 않은 제품 상태입니다."),
     INVALID_PRODUCT_ID("유효하지 않은 제품 ID입니다."),
