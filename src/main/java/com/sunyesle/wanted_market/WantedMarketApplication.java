97f386ba945c80e677dfa9fb4e4907e16685c7ba
sunyesle <sunyesle@gmail.com>
2024-05-30 20:12:10 +0900

    init

---
+++ src/main/java/com/sunyesle/wanted_market/WantedMarketApplication.java
@@ -0,0 +1,13 @@
+package com.sunyesle.wanted_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WantedMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunyesle/wanted_market/WantedMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunyesle.wanted_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
