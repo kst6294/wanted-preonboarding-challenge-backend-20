768d7c8fc53cbe58a0d6271e5cdf6b1174ebbe30
sunyesle <sunyesle@gmail.com>
2024-05-31 00:26:26 +0900

    feat: spring security 의존성 추가

---
+++ src/main/java/com/sunyesle/wanted_market/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.sunyesle.wanted_market.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain api(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        ;
+        return http.build();
+    }
+
+}
