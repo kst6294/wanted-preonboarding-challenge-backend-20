c1374698ae306e20f741f0ec91a20fa952bf3ae8
HanDaWoon <bookpia.han@gmail.com>
2024-05-31 16:43:57 +0900

    feat: csrf 활성화

--- src/main/java/wanted/preonboard/market/config/SecurityConfig.java
+++ src/main/java/wanted/preonboard/market/config/SecurityConfig.java
@@ -8,11 +8,13 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -37,16 +39,21 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new CsrfTokenRequestAttributeHandler();
+        csrfTokenRequestAttributeHandler.setCsrfRequestAttributeName("_csrf");
+
         http
             .securityContext(securityContext -> securityContext.requireExplicitSave(false))
             .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
             .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-            .csrf(AbstractHttpConfigurer::disable)
+            .csrf(csrf -> csrf.csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
+                .ignoringRequestMatchers("/register")
+                .ignoringRequestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.GET, "/products/**"))
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
             .authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/user").authenticated()
-                .requestMatchers(HttpMethod.GET, "/products").permitAll()
-                .requestMatchers("/products", "/products/*").authenticated()
-                .anyRequest().permitAll()
+                .requestMatchers(HttpMethod.POST, "/register").permitAll()
+                .requestMatchers(HttpMethod.GET, "/products/**").permitAll()
+                .anyRequest().authenticated()
             )
             .formLogin(Customizer.withDefaults())
             .httpBasic(Customizer.withDefaults());
