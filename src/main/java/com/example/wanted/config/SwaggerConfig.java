42cfb65e371d79be3a55ba8a0ea21814a9b0e8cf
mini1115 <kmini9611@naver.com>
2024-06-01 14:26:34 +0900

    chore : swagger maven, config 추가

---
+++ src/main/java/com/example/wanted/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.example.wanted.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+    private Info apiInfo(){
+        return new Info()
+                .title("API")
+                .description("Swagger UI")
+                .version("1.0.0");
+    }
+}
