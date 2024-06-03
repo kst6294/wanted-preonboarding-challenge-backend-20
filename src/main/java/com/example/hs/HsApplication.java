c8be680740bb3bdacf85da360c4584d8b963bcd2
HanSeul <w0w1278@naver.com>
2024-06-03 19:11:05 +0900

    Init: Add Spring Boot project files

---
+++ src/main/java/com/example/hs/HsApplication.java
@@ -0,0 +1,13 @@
+package com.example.hs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hs/HsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
