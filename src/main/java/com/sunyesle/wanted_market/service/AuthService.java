0ef42335e237e180683057bae92d4113dc6fc3be
sunyesle <sunyesle@gmail.com>
2024-06-03 22:56:13 +0900

    fix: 누락된 @Transactional 추가

--- src/main/java/com/sunyesle/wanted_market/service/AuthService.java
+++ src/main/java/com/sunyesle/wanted_market/service/AuthService.java
@@ -13,8 +13,10 @@ import com.sunyesle.wanted_market.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class AuthService {
     private final MemberRepository memberRepository;
--- src/main/java/com/sunyesle/wanted_market/service/ProductService.java
+++ src/main/java/com/sunyesle/wanted_market/service/ProductService.java
@@ -10,10 +10,12 @@ import com.sunyesle.wanted_market.exception.ProductErrorCode;
 import com.sunyesle.wanted_market.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class ProductService {
 
