962e2275d02c7bf681483e4194eab7abc1f5e142
Taylor <115taegyeong@gmail.com>
2024-06-04 19:04:47 +0900

    init

---
+++ src/test/java/org/example/wantedpreonboardingchallengebackend20/WantedPreonboardingChallengeBackend20ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.wantedpreonboardingchallengebackend20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedPreonboardingChallengeBackend20ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
