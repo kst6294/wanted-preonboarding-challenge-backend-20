47578df583806fe443ebb70b32e148efe853aed5
Denny-Ko <donghar@naver.com>
2024-06-05 02:48:41 +0900

    [Feat] global exception_handler

---
+++ src/main/java/com/wanted/market/global/web/exceptionhandler/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.wanted.market.global.web.exceptionhandler;
+
+import com.wanted.market.common.exception.UnauthorizedRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(UnauthorizedRequestException.class)
+    public ResponseEntity handleUnauthorizedRequestException(UnauthorizedRequestException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, e.getMessage()));
+    }
+}
