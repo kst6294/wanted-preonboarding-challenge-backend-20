2661f58f609a610b49406a7d7698836bc3df56ad
Denny-Ko <donghar@naver.com>
2024-06-05 02:50:34 +0900

    [Test] authentication
    `+` /login
    `+` /logout

---
+++ src/main/resources/db/init/member.sql
@@ -0,0 +1,5 @@
+INSERT INTO test_db.member (id, username, password) VALUES (1, 'donghar', 'donghar_password');
+INSERT INTO test_db.member (id, username, password) VALUES (2, 'donghar2', 'donghar_password');
+INSERT INTO test_db.member (id, username, password) VALUES (3, 'donghar3', 'donghar_password');
+INSERT INTO test_db.member (id, username, password) VALUES (4, 'donghar4', 'donghar_password');
+INSERT INTO test_db.member (id, username, password) VALUES (5, 'donghar5', 'donghar_password');
