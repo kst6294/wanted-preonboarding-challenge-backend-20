868e93da9a7c1fba66bdb3db969b10c8dbae35cd
김병웅 <quddnddl35@naver.com>
2024-06-07 20:24:08 +0900

    Fix : JPA 직렬화 문제 해결

--- src/main/java/wanted/Market/domain/transaction/entity/Transaction.java
+++ src/main/java/wanted/Market/domain/transaction/entity/Transaction.java
@@ -20,7 +20,7 @@ public class Transaction {
     private String buyerName;
     private boolean status;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id")
     private Product product;
 
