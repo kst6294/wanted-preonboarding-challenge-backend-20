5beab46e7e6a660843567625f7ebb8cab2404975
Taewon Hwang <htw1203@gmail.com>
2024-06-07 23:30:15 +0900

    initial commit

---
+++ src/test/java/com/wantedmarket/WantedPreonboardingChallengeBackend20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wantedmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedPreonboardingChallengeBackend20ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
