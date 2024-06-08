b15423815da11e97a960e03188d1c0f985c07692
qkrrlgus114 <qkrrlgus114@naver.com>
2024-06-08 14:59:36 +0900

    Feat : 아이템 구매 API에 비관적 쓰기락 적용으로 재고 정합성 보장

--- challenge/src/main/java/com/wanted/challenge/domain/item/repository/ItemRepository.java
+++ challenge/src/main/java/com/wanted/challenge/domain/item/repository/ItemRepository.java
@@ -2,7 +2,9 @@ package com.wanted.challenge.domain.item.repository;
 
 import com.wanted.challenge.domain.item.dto.response.ItemResponseDTO;
 import com.wanted.challenge.domain.item.entity.Item;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -15,6 +17,7 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
             "from Item i")
     List<ItemResponseDTO> findAllItems();
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select i from Item i join fetch i.member where i.id = :id")
     Optional<Item> findByIdFetchJoinMember(@Param("id")Long id);
 }
