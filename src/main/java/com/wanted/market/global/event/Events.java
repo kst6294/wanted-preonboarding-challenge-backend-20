8dd4f3574946e9083eab44d0ca420f317dae24e7
donghar <donghar@naver.com>
2024-06-10 19:33:16 +0900

    [Fix] encapsulate constructor and prevent extension

--- src/main/java/com/wanted/market/global/event/Events.java
+++ src/main/java/com/wanted/market/global/event/Events.java
@@ -3,7 +3,7 @@ package com.wanted.market.global.event;
 import com.wanted.market.common.event.Event;
 import org.springframework.context.ApplicationEventPublisher;
 
-public class Events {
+public final class Events {
     private static ApplicationEventPublisher eventPublisher;
 
     public static void publish(Event event) {
--- src/main/java/com/wanted/market/global/web/constance/SessionConst.java
+++ src/main/java/com/wanted/market/global/web/constance/SessionConst.java
@@ -1,5 +1,8 @@
 package com.wanted.market.global.web.constance;
 
-public class SessionConst {
+public final class SessionConst {
     public static final String SESSION_KEY = "authentication";
+
+    private SessionConst() {
+    }
 }
