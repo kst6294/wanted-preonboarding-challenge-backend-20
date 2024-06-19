57386de0a1a0016476f6fe72074f576adfcd133a
q <hoyoung7827@gmail.com>
2024-06-19 18:11:50 +0900

    거래 상태 Enum 추가 및 거래 상태 업데이트 코드 추가

--- market/src/main/java/market/market/domain/transaction/domain/Transaction.java
+++ market/src/main/java/market/market/domain/transaction/domain/Transaction.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import market.market.domain.product.domain.Product;
+import market.market.domain.transaction.enums.TransactionStatus;
 import market.market.global.entity.BaseEntity;
 
 @Entity
@@ -24,6 +25,10 @@ public class Transaction extends BaseEntity {
 
     private int price;
 
+    // 판매 승인, 구매 확정 Enum 클래스 만들기
+    @Enumerated(EnumType.STRING)
+    private TransactionStatus status;
+
     @Builder
     public Transaction(Long buyer_id, String buyer_accountid, Long seller_id, String seller_accountid, Product product, int price) {
         this.buyer_id = buyer_id;
@@ -32,5 +37,10 @@ public class Transaction extends BaseEntity {
         this.seller_accountid = seller_accountid;
         this.product = product;
         this.price = price;
+        this.status = TransactionStatus.UNDEFINED;
+    }
+
+    public void updateStatus(TransactionStatus status) {
+        this.status = status;
     }
 }
