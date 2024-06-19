f8a698407a6057f9617290cbc6bb32e2a5b6514a
q <hoyoung7827@gmail.com>
2024-06-19 18:08:58 +0900

    판매 확정 코드 수정

--- market/src/main/java/market/market/domain/transaction/service/UpdateTransactionService.java
+++ market/src/main/java/market/market/domain/transaction/service/UpdateTransactionService.java
@@ -5,6 +5,7 @@ import market.market.domain.product.domain.Product;
 import market.market.domain.product.enums.Status;
 import market.market.domain.product.facade.ProductFacade;
 import market.market.domain.transaction.domain.Transaction;
+import market.market.domain.transaction.enums.TransactionStatus;
 import market.market.domain.transaction.facade.TransactionFacade;
 import market.market.domain.user.domain.User;
 import market.market.domain.user.facade.UserFacade;
@@ -27,11 +28,10 @@ public class UpdateTransactionService {
         Transaction transaction = transactionFacade.getTransactionById(transactionId);
 
         if (Objects.equals(user.getId(), transaction.getSeller_id())) {
-            Product product = transaction.getProduct();
-            if(product.getStatus() == Status.Completion) { 
-                throw new CustomException(ErrorCode.TRANSACTION_ALREADY_EXISTS);
+            if(transaction.getStatus() == TransactionStatus.APPROVAL) {
+                throw new CustomException(ErrorCode.TRANSACTION_STATUS_ALREADY_EXISTS);
             }
-            product.updateStatus(Status.Completion);
-        }
+            transaction.updateStatus(TransactionStatus.APPROVAL);
+        }else throw new CustomException(ErrorCode.USER_ALREADY_EXISTS);
     }
 }
